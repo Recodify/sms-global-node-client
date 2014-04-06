4b939855decfc09c7725503df1527017c0e0e08c
sam shiles <shilessam@hotmail.com>
2014-04-06 19:02:56 +0100

    workign with callback

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var sms = require('./lib/wrapper');
 
 var wrapper = new sms.wrapper('8054c0ec196e62be6e85faf406032967', '0db8cc05b02d2a6bda3fd32ea8789a53');
-wrapper.get('group',  function(body){
-     
+wrapper.get('contact', null,  function(body){
+	console.log(body);
 });
