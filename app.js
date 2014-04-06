dcbbdb97379a2e3f4ae7029495fb12ad4888ad06
sam shiles <shilessam@hotmail.com>
2014-04-06 20:38:15 +0100

    fixing up nasty merge

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var sms = require('./lib/wrapper');
-
+/*
 var wrapper = new sms.wrapper('null', 'null');
 wrapper.get('contact', null,  function(body){
 	console.log(body);
 });*/
 
+var wrapper = new sms.wrapper('8054c0ec196e62be6e85faf406032967', '0db8cc05b02d2a6bda3fd32ea8789a53');
 var message = {
 	origin: "test",
 	destination: "447850229403",
