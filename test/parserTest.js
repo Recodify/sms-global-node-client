b4846d27b53a00fc1a8781685c7dc7fa2cfa2ada
sam shiles <shilessam@hotmail.com>
2014-04-06 14:05:25 +0100

    testing

--- test/parserTest.js
+++ test/parserTest.js
@@ -2,6 +2,7 @@ var assert = require('assert')
 var request = require('request');
 var fakeweb = require('node-fakeweb');
 var testUrl = 'http://www.testing.com:80/';
+var sms = require('../lib/wrapper');
 
 beforeEach(function(){
 	var output = function(err, resp, body) { console.log(body); }
@@ -14,8 +15,10 @@ beforeEach(function(){
 describe('scapper', function(){
 	describe('get', function(){
     	it('should return jquery object with expected content', function(done){
-        	assert.equal(1, 1);
-      		done();
+        	var wrapper = new sms.wrapper('8054c0ec196e62be6e85faf406032967', '0db8cc05b02d2a6bda3fd32ea8789a53');
+      		wrapper.get('groups', function(body){
+      			done();
+      		});
     	})
   	})
 });
